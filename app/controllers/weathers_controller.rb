343f992124ba4a5057f3011580368a1872306c2f
liliya-tagirova <tor-ver@mail.ru>
2022-08-28 00:08:06 +0300

    feat: add weather model and controller

---
+++ app/controllers/weathers_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class WeathersController < ApplicationController
+  def current
+    render json: '123'
+  end
+
+  def historical
+    render json: '123'
+  end
+
+  def historical_max
+    render json: '123'
+  end
+
+  def historical_min
+    render json: '123'
+  end
+
+  def historical_avg
+    render json: '123'
+  end
+
+  def by_time
+    render json: '123'
+  end
+end
