17dc943cbf1512709d87dd542eba6eb075fda1a6
liliya-tagirova <tor-ver@mail.ru>
2022-08-29 08:29:51 +0300

    fix: schema

--- db/migrate/20220829032943_create_weathers.rb
+++ db/migrate/20220829032943_create_weathers.rb
@@ -4,7 +4,7 @@ class CreateWeathers < ActiveRecord::Migration[6.1]
   def change
     create_table :weathers do |t|
       t.datetime :weather_time
-      t.integer :temperature
+      t.float :temperature
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2022_08_29_032943) do
 
   create_table "weathers", force: :cascade do |t|
     t.datetime "weather_time"
-    t.integer "temperature"
+    t.float "temperature"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
