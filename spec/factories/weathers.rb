32bc636b8eded547e171ec4f80771852d0b25969
liliya-tagirova <tor-ver@mail.ru>
2022-08-30 05:49:33 +0300

    add spec and swagger for requests

--- spec/factories/weathers.rb
+++ spec/factories/weathers.rb
@@ -2,8 +2,8 @@
 
 FactoryBot.define do
   factory :weather do
-    local_time { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) }
-    epoch_time { 1_234_567_890 }
-    temperature { 23.5 }
+    local_time { Faker::Time.between(from: DateTime.now - 5, to: DateTime.now) }
+    epoch_time { local_time.to_i }
+    temperature { (-20..20).to_a.sample }
   end
 end
