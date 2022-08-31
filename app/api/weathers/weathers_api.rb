857f3b05f4957307b92be0cf8bbe2121eaf98bae
liliya-tagirova <tor-ver@mail.ru>
2022-08-31 11:01:11 +0300

    change routing to grape

--- app/api/weathers/weathers_api.rb
+++ app/api/weathers/weathers_api.rb
@@ -37,29 +37,3 @@ module Weathers
     end
   end
 end
-
-
-
-# def current
-#   render json: Weather.current_weather
-# end
-
-# def historical
-#   render json: Weather.last_weathers
-# end
-
-# def historical_max
-#   render json: Weather.max
-# end
-
-# def historical_min
-#   render json: Weather.min
-# end
-
-# def historical_avg
-#   render json: Weather.avg
-# end
-
-# def by_time
-#   render json: Weather.by_time(params[:unix_time])
-# end
